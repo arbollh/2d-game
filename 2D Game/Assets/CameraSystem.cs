2782fcfcd679d2a1b1a1b0b2a04c670236d816d6
Ashley B <arbollh@ilstu.edu>
2019-04-19 16:02:31 -0500

    Adding camera and reseting scene at player death from falling

---
+++ 2D Game/Assets/CameraSystem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSystem : MonoBehaviour {
+
+
+	public GameObject player;
+
+	// Variables that will clamp the camera in a certain position
+	public float xMin; // Least it can go on the x plane
+	public float xMax; // Most it can go on the x plane
+	public float yMin; // Least it can go on the y plane
+	public float yMax; // Most it can go on the y plane
+
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		float x = Mathf.Clamp (player.transform.position.x, xMin, xMax);
+		float y = Mathf.Clamp (player.transform.position.y, yMin, yMax);
+		gameObject.transform.position = new Vector3 (x, y, gameObject.transform.position.z);
+	}
+}
